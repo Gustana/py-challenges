ff050179860a06fe289a318d737b0a3ebba2547f
gustana <gustana.satiawan@gmail.com>
2019-03-16 16:05:40 +0800

    helper for sqlite operation

--- schedule/helper.py
+++ schedule/helper.py
@@ -1,7 +1,22 @@
 def insertSchedule(conn, schedule) :
-    query = ''' INSERT INTO schedule(activity, time, day) 
+        query = ''' INSERT INTO schedule(activity, time, day) 
             VALUES(?, ?, ?)'''
+        cursor = conn.cursor()
+        cursor.execute(query, schedule)
+        return True
 
-    cursor = conn.cursor()
-    cursor.execute(query, schedule)
-    return True
+def selectSchedule(conn, query) :
+        cursor = conn.cursor()
+        cursor.execute(query)
+        
+        rows = cursor.fetchall()
+
+        return rows
+
+def removeShcedule(conn, id) :
+        query = ''' DELETE FROM schedule WHERE id = ? '''
+        cursor = conn.cursor()
+        cursor.execute(query, (id,))
+
+        return True
+                
