9e5a15f86da8a957a9c5d8ca14a732695f89f6f1
gustana <gustana.satiawan@gmail.com>
2019-03-16 16:07:00 +0800

    operation for add schedule

--- schedule/makeSchedule.py
+++ schedule/makeSchedule.py
@@ -1,27 +1,27 @@
 from helper import insertSchedule
 from connection import create_connection
 
-db_file = "E:/Program/Python/schedule/db_schedule.db"
-
 dayList = ("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
-i = 1
-while i<7 :
-    print(i, end=".")
-    print(dayList[i-1], sep='\n')
-    i+=1
 
-day = int(input("Select day : "))
-time = input("Insert time :")
-activity = input("What do you want to do ? :")
+def makeSchedule() :
+    i = 1
+    while i<7 :
+        print(i, end=".")
+        print(dayList[i-1], sep='\n')
+        i+=1
 
-schedule = (activity, time, day)
+    day = int(input("Select day : "))
+    time = input("Insert time :")
+    activity = input("What do you want to do ? :")
 
-conn = create_connection(db_file)
-with conn :
-    query = insertSchedule(conn, schedule)
-    if query :
-        print("Success")
-    else :
-        print("Failed")
+    schedule = (activity, time, day)
+
+    conn = create_connection()
+    with conn :
+        query = insertSchedule(conn, schedule)
+        if query :
+            print("Success")
+        else :
+            print("Failed")
 
     
