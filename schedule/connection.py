a823cab73266908d18fa73d9282812db1cd2f2d3
gustana <gustana.satiawan@gmail.com>
2019-03-16 16:05:00 +0800

    connection for sqlite3

--- schedule/connection.py
+++ schedule/connection.py
@@ -1,7 +1,7 @@
 import sqlite3
 from sqlite3 import Error
 
-def create_connection(db_file):
+def create_connection(db_file = "E:/Program/Python/Schedule/db_schedule.db"):
     #""" create a database connection to a SQLite database """
     try:
         conn = sqlite3.connect(db_file)
